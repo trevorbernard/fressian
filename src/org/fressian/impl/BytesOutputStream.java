a4ecf373c7746775f7fd0ed7c160957ec0b9a02f
Trevor Bernard <trevor.bernard@userevents.com>
2013-05-26 14:51:18 -0300

    Modify BytesOutputStream to remove thread safety

--- src/org/fressian/impl/BytesOutputStream.java
+++ src/org/fressian/impl/BytesOutputStream.java
@@ -9,7 +9,10 @@
 package org.fressian.impl;
 
 import java.io.ByteArrayOutputStream;
-
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
+import java.util.Arrays;
 
 public class BytesOutputStream extends ByteArrayOutputStream {
     public BytesOutputStream() {
@@ -27,4 +30,62 @@ public class BytesOutputStream extends ByteArrayOutputStream {
         return count;
     }
 
+    private void ensureCapacity(int minCapacity) {
+        // overflow-conscious code
+        if (minCapacity - buf.length > 0)
+            grow(minCapacity);
+    }
+
+    public byte toByteArray()[] {
+        return Arrays.copyOf(buf, count);
+    }
+
+    private void grow(int minCapacity) {
+        // overflow-conscious code
+        int oldCapacity = buf.length;
+        int newCapacity = oldCapacity << 1;
+        if (newCapacity - minCapacity < 0)
+            newCapacity = minCapacity;
+        if (newCapacity < 0) {
+            if (minCapacity < 0) // overflow
+                throw new OutOfMemoryError();
+            newCapacity = Integer.MAX_VALUE;
+        }
+        buf = Arrays.copyOf(buf, newCapacity);
+    }
+
+    public void write(int b) {
+        ensureCapacity(count + 1);
+        buf[count] = (byte) b;
+        count += 1;
+    }
+
+    public void write(byte b[], int off, int len) {
+        if ((off < 0) || (off > b.length) || (len < 0) || ((off + len) - b.length > 0)) {
+            throw new IndexOutOfBoundsException();
+        }
+        ensureCapacity(count + len);
+        System.arraycopy(b, off, buf, count, len);
+        count += len;
+    }
+
+    public void reset() {
+        count = 0;
+    }
+
+    public int size() {
+        return count;
+    }
+
+    public String toString() {
+        return new String(buf, 0, count);
+    }
+
+    public String toString(String charsetName) throws UnsupportedEncodingException {
+        return new String(buf, 0, count, charsetName);
+    }
+
+    public void writeTo(OutputStream out) throws IOException {
+        out.write(buf, 0, count);
+    }
 }
